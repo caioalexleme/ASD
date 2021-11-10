1eb254a512a84dfc863a2a0f9f41595f28b877c6
Caio <caioalexleme@hotmail.com>
2021-11-09 22:28:35 -0300

    trains

--- PIM/pim4/index.c
+++ PIM/pim4/index.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main () {
+    char nome[90];
+    char opcao;
+    printf("Digite seu nome: ");
+    scanf(" %s ", &nome);
+    printf("%s Deseja continuar? ", nome);
+    scanf(" %c ", &opcao);
+    if(opcao == "n"){
+        printf("\nAté mais!!");
+    }else{
+        scanf(" %c ", &opcao);
+    }
 
-    printf("Nome: \n");
 
+
+    system("PAUSE");
+    return 0;
 }
