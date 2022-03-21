ceed8e9eccf50bdecefd1a42ea6fc352fff4ef16
Caio <caioalexleme@hotmail.com>
2022-03-20 21:46:08 -0300

    c#

---
+++ 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primeiro_Programa
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá mundo em C# !!!");
+        Console.ReadLine();
+    }
+}
+}
