3dead6ef96adc95d2c0822440f6b9558bc1f939e
Caio <caioalexleme@hotmail.com>
2022-03-22 21:56:11 -0300

    Primeiro programa em C#

--- 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp1/Program.cs
+++ 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp1/Program.cs
@@ -12,6 +12,24 @@ namespace Primeiro_Programa
     {
         Console.WriteLine("Olá mundo em C# !!!");
         Console.ReadLine();
+            string nome = "Caio";
+            string sobrenome = " Alexandre";
+            string str = nome + sobrenome;
+
+            Console.WriteLine("str = {0}", str);
+            
+            Console.WriteLine("Olá qual o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("E Qual a sua idade?");
+            int idade;
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("{0}, Você tem {1} anos de idade.", nome, idade);
+            Console.ReadKey();
+
+        }
     }
-}
 }
