2db83660e26f76f9ac9faf54b1af08d0add2fe93
Caio <caioalexleme@hotmail.com>
2022-03-23 22:27:43 -0300

    update

--- 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp2/Program.cs
+++ 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp2/Program.cs
@@ -1,26 +1,28 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public class Class1
+namespace Primeiro_Programa
 {
-	public Class1()
+	class Program
 	{
 		static void Main(string[] args)
-        {
+		{
 
-        
+
 			double numero = 0;
 			Console.Write("Digite um número: ");
 			numero = Convert.ToDouble(Console.ReadLine());
 
-			if(numero % 2 == 0)
-            {
+			if (numero % 2 == 0)
+			{
 				Console.WriteLine("par");
-            }
-            else
-            {
+			}
+			else
+			{
 				Console.WriteLine("impar");
 			}
 			Console.ReadKey();
