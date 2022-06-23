2eb4050d084ada91b6c522a645748062b37a2d31
Caio <caioalexleme@hotmail.com>
2022-06-23 16:19:59 -0300

    teste

---
+++ 2semestre/Programação orientada a objetos I/unidade II/ConsoleApp2/parimpar/Form1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primeiro_Programa
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+			double numero = 0;
+			Console.Write("Digite um número: ");
+			numero = Convert.ToDouble(Console.ReadLine());
+
+			if (numero % 2 == 0)
+			{
+				Console.WriteLine("par");
+			}
+			else
+			{
+				Console.WriteLine("impar");
+			}
+			Console.ReadKey();
+		}
+	}
+}
